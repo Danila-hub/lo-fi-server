9e414ea0d4116b4be57c945616ebb86d02fca44d
Danila-hub <kontaigabox@mail.ru>
2021-01-29 09:13:36 +0300

    replace files from old rep

---
+++ js/button_burger.js
@@ -0,0 +1,12 @@
+let $burgerButton = document.querySelector('.header__burger')
+let $burgerMenu = document.querySelector('.header__burger-menu')
+let $burgerCloseButton = document.querySelector('.header__burger-menu-close')
+let body = document.querySelector('body')
+$burgerButton.addEventListener('click', function () {
+  $burgerMenu.style.top = '0px'
+  body.style.overflow = 'hidden'
+})
+$burgerCloseButton.addEventListener('click', function () {
+  $burgerMenu.style.top = '-999px'
+  body.style.overflow = 'auto'
+})
